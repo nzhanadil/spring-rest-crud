1c0dbcc0975533b7c17b262d3887343f32151c7a
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-09-12 23:58:16 -0500

    added logic for getting specific student based on index

--- src/main/java/com/spring_rest/demo/rest/StudentRestController.java
+++ src/main/java/com/spring_rest/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.spring_rest.demo.rest;
 
 import com.spring_rest.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +14,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for /students
+    private List<Student> students;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once in the beginning.
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
 
         students.add(new Student("Mike", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Marry", "Smith"));
+    }
 
+    // define endpoint for /students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    // define endpoint for /students/{studentId}
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
